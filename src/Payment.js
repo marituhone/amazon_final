8c939bf62274fa3f8071a623ce54cb8a43d2e3c7
marituhone <marehone12@gmail.com>
2023-01-20 23:56:17 +0300

    component of payment

--- src/Payment.js
+++ src/Payment.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import CheckoutProduct from './CheckoutProduct';
 import './Payment.css'
+import { useStateValue } from './StateProvider';
 
 function Payment() {
+
+    const[{basket,user},dispatch] = useStateValue(); 
   return (
     <div className='payment'>
          <div className="payment__container">
@@ -10,10 +14,31 @@ function Payment() {
                     <h3>Delivery information</h3>
 
                 </div>
+                <div className="payment__address">
+                    <p>{user?.email}em</p>
+                    <p>react 123</p>
+                    <p>Losangels </p>
+                </div>
 
             </div>
 
             <div className="payment__section">
+                <div className="payment__title">
+                    <h3>reveiew items and delivery</h3>
+                </div>
+                <div className="payment__items">
+                    
+                    {basket.map(item =>
+                        {
+                            <CheckoutProduct
+                            id = {item.id}
+                            title ={item.title}
+                            image = {item.image}
+                            price = {item.price}
+                            rateing = {item.rateing}
+                             />
+                        })}
+                </div>
                 
             </div>
 
